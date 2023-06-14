c4b3febb261e9942a5e7f9d0aae7622e1e336147
PerezFranco <40713926+PerezFranco@users.noreply.github.com>
2023-06-14 19:40:44 +1000

    Update replace.f

--- s/replace.f
+++ s/replace.f
@@ -1,16 +1,1 @@
-(given,old,new)
-Func
-Local revised,still,len
-given→revised
-dim(old)→len
-inString(revised,old)→still
 
-While still≠0
-
-left(revised,still-1)&new&mid(revised,still+len)→revised
-
-inString(revised,old)→still
-EndWhile
-
-Return revised
-EndFunc
